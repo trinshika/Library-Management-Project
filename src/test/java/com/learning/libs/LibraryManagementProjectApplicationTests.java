49f9e637208acc48cf30528e80b34ed1f15063fc
Trinshika Yadav <trinshika@gmail.com>
2026-05-28 15:24:49 +0530

    first commit

---
+++ src/test/java/com/learning/libs/LibraryManagementProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.libs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
